8e0d4b02728043261e42fd05d98c8be65e04905d
Wilber Ulloa <wuj890312@gmail.com>
2020-10-20 22:57:10 -0400

    added react-router-dom dependency

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgv2CUiZwl7GbGjarWiswbFaJvs2-PX00",
+    authDomain: "slack-clone-f90cb.firebaseapp.com",
+    databaseURL: "https://slack-clone-f90cb.firebaseio.com",
+    projectId: "slack-clone-f90cb",
+    storageBucket: "slack-clone-f90cb.appspot.com",
+    messagingSenderId: "775835116408",
+    appId: "1:775835116408:web:9684ac6f39e2b985d11e8d",
+    measurementId: "G-RXHGD0Q5GS"
+  };
