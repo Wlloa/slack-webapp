ef54c7062af95810185ff709bd30db72e21f7495
Wilber Ulloa <wuj890312@gmail.com>
2020-10-22 17:21:53 -0400

    initialized router dom

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import {Switch, BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
+    <Router>
       {/* Header */}
       <Header/>
       <div className="app__body">
         {/* Sidebar */}
         <Sidebar/>
+        
+          <Switch>
+            <Route path="/"/>
+            <Route path="/room/:roomId"/>
+          </Switch>
+        
         {/* React Router ---> chat screen */}
       </div>
       
-
+    </Router>
     </div>
   );
 }
--- src/components/sidebar_item/SidebarItem.js
+++ src/components/sidebar_item/SidebarItem.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import db from '../../firebase';
 import './SidebarItem.css';
+import {useHistory} from 'react-router-dom';
 
-function SidebarItem({Icon, title, allowAddition=false}) {
+function SidebarItem({Icon, title, id, allowAddition=false}) {
+
+    const history = useHistory();
 
     const addChannel = ()=>{
         const channelName = prompt("Enter channel's name");
-        console.log(channelName);
-        db.collection('rooms').add({
-            name:channelName,
-        }).then((docRef)=>{
-            console.log('Document created with ID: ', docRef.id)
-        }).catch((error)=>{
-            console.log("Error adding document: ", error)
-        })
+        if(channelName.length > 0){
+            console.log(channelName);
+            db.collection('rooms').add({
+                name:channelName,
+            }).then((docRef)=>{
+                console.log('Document created with ID: ', docRef.id)
+            }).catch((error)=>{
+                console.log("Error adding document: ", error)
+            })
+        }
+        
+    }
+
+    const changeChannel = ()=>{
+        history.push(`/rooms/${id}`)
     }
 
     return (
-        <div onClick={allowAddition?addChannel:null} className="sidebarItem">
+        <div onClick={allowAddition?addChannel:changeChannel} className="sidebarItem">
             {Icon && <Icon className="sidebarItem__icon"/>}
             {Icon ? (
                 <h3>{title}</h3>
